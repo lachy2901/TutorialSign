296bf9b6261fc0eba3a2cd236c957b09f2818a33
Lachlan H <lachy2901@gmail.com>
2011-12-02 14:34:09 +1030

    Added commands /tut stop and /tut play <category> <content>

--- src/main/java/com/precipicegames/tutorialsign/TutorialSign.java
+++ src/main/java/com/precipicegames/tutorialsign/TutorialSign.java
@@ -4,10 +4,17 @@ import java.io.File;
 import java.io.IOException;
 import java.util.logging.Logger;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.getspout.spoutapi.SpoutManager;
+import org.getspout.spoutapi.player.SpoutPlayer;
 
 public class TutorialSign extends JavaPlugin{
 
@@ -37,5 +44,31 @@ public class TutorialSign extends JavaPlugin{
 	public void onDisable(){
 		final PluginDescriptionFile pdffile = this.getDescription();
 		this.logger.info("Plugin" + pdffile.getName() + " version " + pdffile.getVersion() + " is now disabled.");
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		SpoutPlayer splayer = (SpoutPlayer) sender;
+		File confFile = new File("tutorials.yml");
+		FileConfiguration config = YamlConfiguration.loadConfiguration(confFile);
+		if (cmd.getName().equalsIgnoreCase("tut")){
+			if (args[0] != null){
+				if (args[0].equalsIgnoreCase("stop")){
+
+					SpoutManager.getSoundManager().stopMusic(splayer);
+					splayer.sendMessage(ChatColor.DARK_GREEN + "The tutorial has stopped playing.");
+					return true;
+				}else if (args[0].equalsIgnoreCase("play")){
+					if (args[1] != null && args[2] != null){
+						if (splayer.isSpoutCraftEnabled()){
+							String url = config.getString("tutorials.categories." + args[1] + "." + args[2] + ".url");
+							SpoutManager.getSoundManager().playCustomMusic(plugin, splayer, url, true);
+						}else{
+							splayer.sendMessage(ChatColor.RED + "You need SpoutCraft to listen to tutorials!");
+						}
+					}
+				}
+			}
+		}
+	return false;
 	}
 }
